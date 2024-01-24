4ba8c528d7dc56baedc81dcae57a6ed3869272f1
Joel John <77858020+joel2982@users.noreply.github.com>
2024-01-24 10:37:09 +0530

    Update Q25TypeOfChar.py

--- Q25TypeOfChar.py
+++ Q25TypeOfChar.py
@@ -1,8 +1,8 @@
 char = input('Enter a character = ')
-special_char = '[@_!#$%^&*()<>?/\|}{~:]'  
+special_char = '[@_!#$%^&*().<>?/\|}{~:]'  
 if char.islower(): 
     print('Lowercase')
 elif char.isupper(): 
     print('Uppercase')
 elif char in special_char: 
-    print('Special char')
+    print('Special Character')
