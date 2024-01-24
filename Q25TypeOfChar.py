457dcc6e24f3f99496603b0051e632efb39bea45
Joel John <77858020+joel2982@users.noreply.github.com>
2024-01-24 10:33:52 +0530

    Add files via upload

---
+++ Q25TypeOfChar.py
@@ -0,0 +1,8 @@
+char = input('Enter a character = ')
+special_char = '[@_!#$%^&*()<>?/\|}{~:]'  
+if char.islower(): 
+    print('Lowercase')
+elif char.isupper(): 
+    print('Uppercase')
+elif char in special_char: 
+    print('Special char')
